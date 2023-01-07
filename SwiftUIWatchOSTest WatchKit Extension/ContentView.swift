f11f84dbf1c48897e0893357d6b95445b0977c23
Rob Napier <ornapier@audible.com>
2023-01-07 10:41:12 -0500

    Simple AVAudioPlayerNode version w/ minimal changes

--- SwiftUIWatchOSTest WatchKit Extension/ContentView.swift
+++ SwiftUIWatchOSTest WatchKit Extension/ContentView.swift
@@ -5,22 +5,13 @@ struct ContentView: View {
     @ObservedObject var settings = Settings.shared
 
     init() {
-        settings.engine = .init()
-
         settings.prepare()
     }
 
     var body: some View {
         Button("Play Sound") {
             SoundFontHelper.sharedInstance().playSound()
-
-            if !settings.engine.isRunning {
-                do {
-                    try settings.engine.start()
-                } catch {
-                    print(error)
-                }
-            }
+            settings.play()
         }
     }
 }
--- SwiftUIWatchOSTest WatchKit Extension/Settings.swift
+++ SwiftUIWatchOSTest WatchKit Extension/Settings.swift
@@ -4,57 +4,64 @@ import AVFoundation
 class Settings: ObservableObject {
     static let shared = Settings()
 
-    var engine: AVAudioEngine!
-    var sourceNode: AVAudioSourceNode!
-    var wavData: Data!
+    var engine = AVAudioEngine()
+    let playerNode = AVAudioPlayerNode()
 
     func prepare() {
-        if let sourceNode = sourceNode {
-            engine.detach(sourceNode)
+        engine.attach(playerNode)
+        engine.connect(playerNode, to: engine.mainMixerNode, format: nil)
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+            try engine.start()
+            playerNode.play()
+        } catch {
+            print(error)
         }
+    }
 
-        let mixerNode = engine.mainMixerNode
+    func play() {
+        // This is the output format. We'll need to get to this eventually.
+        let outputFormat = engine.mainMixerNode.outputFormat(forBus: 0)
 
+        // It would be best to generate this in the output format from the start.
+        // I don't know if this library can do that, so I'll convert it to have a general solution.
+        // Generally you'd create this format object once and cache it, but I'm keeping all the code together.
         let audioFormat = AVAudioFormat(
             commonFormat: .pcmFormatInt16,
             sampleRate: 44100,
             channels: 1,
             interleaved: false
-        )
+        )!
 
-        guard let audioFormat = audioFormat else {
-            return
+        // These tones have a long falloff, so you want a lot of source data. This is 10s.
+        let frameCount = 10 * Int(audioFormat.sampleRate)
+
+        // Get the samples
+        let data = SoundFontHelper.sharedInstance().getSound(Int32(frameCount))!
+
+        // Copy the samples into an audio buffer. It would be better to do this directly in getSound, rather
+        // than creating a Data, and then copying it, but this is pretty straightforward.
+        let buffer = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: AVAudioFrameCount(frameCount))!
+        data.withUnsafeBytes { bytes in
+            buffer.frameLength = buffer.frameCapacity
+            let audioBuffer = buffer.audioBufferList.pointee.mBuffers
+            audioBuffer.mData?.copyMemory(from: bytes.baseAddress!, byteCount: Int(audioBuffer.mDataByteSize))
         }
 
-        sourceNode = AVAudioSourceNode(format: audioFormat) { silence, timeStamp, frameCount, audioBufferList in
-            guard let data = SoundFontHelper.sharedInstance().getSound(Int32(frameCount)) else {
-                return 1
-            }
+        // Now convert it with an AVAudioConverter
+        let outputBuffer = AVAudioPCMBuffer(pcmFormat: outputFormat, frameCapacity: AVAudioFrameCount(frameCount))!
 
-            let ablPointer = UnsafeMutableAudioBufferListPointer(audioBufferList)
-
-            data.withUnsafeBytes { (intPointer: UnsafePointer<Int16>) in
-                for index in 0 ..< Int(frameCount) {
-                    let value = intPointer[index]
-
-                    // Set the same value on all channels (due to the inputFormat, there's only one channel though).
-                    for buffer in ablPointer {
-                        let buf: UnsafeMutableBufferPointer<Int16> = UnsafeMutableBufferPointer(buffer)
-                        buf[index] = value
-                    }
-                }
-            }
-
-            return noErr
+        // It should be impossible for this conversion to fail. This could be done with a simpler call
+        // to `convert(to:from:)` if you make sure to create the audio at the right sampling rate from outputFormat.
+        // This version of the call works for any supported format.
+        // It's usually best to create this converter once, and cache it, but this approach keeps the code simpler.
+        let converter = AVAudioConverter(from: audioFormat, to: outputFormat)!
+        converter.convert(to: outputBuffer, error: nil) { inNumPackets, outStatus in
+            outStatus.pointee = .haveData
+            return buffer
         }
 
-        engine.attach(sourceNode)
-        engine.connect(sourceNode, to: mixerNode, format: audioFormat)
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback)
-        } catch {
-            print(error)
-        }
+        // All done. Play the buffer, interrupting whatever is currently playing
+        playerNode.scheduleBuffer(outputBuffer, at: nil, options: .interrupts)
     }
 }
